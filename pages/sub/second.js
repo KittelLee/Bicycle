ccb46d6badbdb6d2a4adf393629f2b3ab6a386da
Leejinuk <leejin7900@gmail.com>
2022-11-24 16:22:33 +0900

    ver0.0.8

---
+++ pages/sub/second.js
@@ -0,0 +1,198 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+export default function second() {
+  return (
+    <Layout>
+      <>
+        <div className="flex justify-between px-20 py-10">
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/manello.jpeg" className="max-w-xl" />
+              <p className="text-center text-lg font-bold">마넬로</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/watts.jpeg" />
+              <p className="text-center text-lg font-bold">
+                기흥인터내셔널(와츠)
+              </p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/neofly.jpeg" />
+              <p className="text-center text-lg font-bold">네오플라이</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/dongjin.jpeg" />
+              <p className="text-center text-lg font-bold">동진임포츠</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/daejin.png" />
+              <p className="text-center text-lg font-bold">대진인터네셔널</p>
+            </div>
+          </Link>
+        </div>
+
+        <div className="flex justify-between px-20 py-10">
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/sanbada.jpeg" />
+              <p className="text-center text-lg font-bold">산바다스포츠</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/sportson55.jpeg" />
+              <p className="text-center text-lg font-bold">스포츠온55</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/cephas.jpeg" />
+              <p className="text-center text-lg font-bold">세파스</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/exo.jpeg" />
+              <p className="text-center text-lg font-bold">이엑스오</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/odbike.jpeg" />
+              <p className="text-center text-lg font-bold">오디바이크</p>
+            </div>
+          </Link>
+        </div>
+
+        <div className="flex justify-between px-20 py-10">
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/glnco.jpeg" />
+              <p className="text-center text-lg font-bold">지엘앤코</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/elfama.png" />
+              <p className="text-center text-lg font-bold">엘파마</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/giant.jpeg" />
+              <p className="text-center text-lg font-bold">자이언트</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/wixx.png" />
+              <p className="text-center text-lg font-bold">윅스인터내셔널</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/cello.png" />
+              <p className="text-center text-lg font-bold">첼로스포츠</p>
+            </div>
+          </Link>
+        </div>
+
+        <div className="flex justify-between px-20 py-10">
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/tradelab.jpeg" />
+              <p className="text-center text-lg font-bold">트레이드랩</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/comete.jpeg" />
+              <p className="text-center text-lg font-bold">코메트바이시클</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/scott.png" />
+              <p className="text-center text-lg font-bold">스캇</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/canyon.png" />
+              <p className="text-center text-lg font-bold">캐니언</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/special.png" />
+              <p className="text-center text-lg font-bold">스페셜라이즈드</p>
+            </div>
+          </Link>
+        </div>
+
+        <div className="flex justify-between px-20 py-10">
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/trek.png" />
+              <p className="text-center text-lg font-bold">트렉코리아</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/felt.png" />
+              <p className="text-center text-lg font-bold">펠트바이시클</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/pro.png" />
+              <p className="text-center text-lg font-bold">프로웍스</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/velo.jpeg" />
+              <p className="text-center text-lg font-bold">벨로직</p>
+            </div>
+          </Link>
+
+          <Link href={"http://www.manello.co.kr/"} passHref>
+            <div className="bg-gray-200 p-2 rounded">
+              <img src="../logo/insa.jpeg" />
+              <p className="text-center text-lg font-bold">인세인바이크</p>
+            </div>
+          </Link>
+        </div>
+      </>
+    </Layout>
+  );
+}
